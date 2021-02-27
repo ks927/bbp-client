40c7cfe79e8beb316e1b988d4d4eefb6cc7d1e89
Ksheehan <ksheehan927@gmail.com>
2021-02-26 23:37:09 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react/cjs/react.development';
 
-function App() {
+const App: React.FC = () => {
+  const [name, setName] = useState('');
+
+  const onClick = () => {
+    fetch('http://localhost:8080/login').then((res) => {
+      console.log('fetchingggg', res)
+      return res.json()
+    }).then((data) => {
+      console.log('aadfasdfadsdaatttaaaa', data.data[0].name)
+      setName(data.data[0].name);
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to Bran's Plans, {name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" onClick={onClick}>
+          ok whatever
         </a>
       </header>
     </div>
